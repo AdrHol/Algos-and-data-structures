4d4b127fd5cba17f8d30d814810e28ac648e45fe
adi1111 <holubeckiadrian@gmail.com>
2022-10-30 17:39:20 +0100

    adding some methods

--- BTS/binaryTree.js
+++ BTS/binaryTree.js
@@ -1,5 +1,5 @@
 class binaryTree { 
-    constructor(initalArr){
+    constructor(initalArr = null){
         this.arr = initalArr;
         this.root = null;
     }
@@ -17,40 +17,53 @@ class binaryTree {
     }
 
     buildTree(){
+
         let sortedArray = this.sort();
 
-        function bigRecursion(arr){
+        function decomposition(arr){
              if (arr.length === 1) {
             return new node(arr[0], null, null);
-
         } else if (arr.length === 2 ){
-            return new node(arr[1], bigRecursion([arr[0]]), null);
+            return new node(arr[0], null , decomposition([arr[1]]));
         } else {
             let mid = Math.floor(arr.length/2);
             let left = arr.slice(0, mid);
             let right = arr.slice(mid + 1, arr.length);
-            
-            return new node(arr[mid], bigRecursion(left), bigRecursion(right));
+            return new node(arr[mid], decomposition(left), decomposition(right));
+        }
+        }
+        this.root = decomposition(sortedArray);
+    }
+    insert(value){
+      if (this.root === null) throw 'Root is empty';
+        
+        function rec(value, root){
+            if (root === null){
+                root = new node(value, null, null);
+                return root;
+            }
+            if(value < root.data){
+                root.left = rec(value, root.left);
+            } else if (value > root.data){
+                root.right = rec(value,root.right);
+            }
+            return root;
+        }
 
-        }
-        }
-        console.log(sortedArray);
-        this.root = bigRecursion(sortedArray);
+      this.root = rec(value, this.root);
+
 
     }
+
 }
 
-
-
-
 class node {
-    constructor(data, left, right){
+    constructor(data, left = null, right = null){
         this.data = data;
         this.left = left;
         this.right = right;
     }
 }
-
 
 prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node.right !== null) {
